24d7febe98b2714d3e7b53afab6d529e01156c10
DavidRiveraArtero <davidtoy222@gmail.com>
2021-07-07 10:32:10 +0200

    UD4-Ej2

---
+++ UD4_OperacionesEj2App.java
@@ -0,0 +1,27 @@
+
+public class UD4_OperacionesEj2App {
+
+	public static void main(String[] args) {
+		
+		int N = 20;
+		
+		double A = 10.2;
+		
+		char C = 'a';
+		
+		//MOSTRAMOS LOS 3 VALORES 
+		System.out.println("El valor de N es: "+ N);
+		System.out.println("El valor de A es: "+A);
+		System.out.println("El valor de C es: "+ (int)C);
+
+		//OPERACIONES
+		System.out.println("=============================");
+		double resultado= N + A;
+		System.out.println("LA SUMA ES: "+resultado);
+		
+		System.out.println(A - N);
+		System.out.println((int)C);
+		
+	}
+
+}
